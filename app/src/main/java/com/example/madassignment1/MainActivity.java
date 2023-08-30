663542dd631ce05266299ddcf0c12d1c7478e150
Karthik Karavatt <karthikkaravatt@gmail.com>
2023-08-30 15:20:49 +0800

    Accidentally committed the MainActivity, changed it back to its normal state

--- app/src/main/java/com/example/madassignment1/MainActivity.java
+++ app/src/main/java/com/example/madassignment1/MainActivity.java
@@ -6,11 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private GameSettings gameSettings = new GameSettings();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportFragmentManager().beginTransaction().replace(R.id.MainActivityFrameLayout, gameSettings).commit();
     }
 }
