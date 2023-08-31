3985248c16228d7e76b0383de0e5c690ae4d8a09
ameliefisher <amelie.fisher@student.curtin.edu.au>
2023-08-31 15:04:51 +0800

    Added a few navigation features (TBC)

--- app/src/main/java/com/example/madassignment1/MainActivity.java
+++ app/src/main/java/com/example/madassignment1/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.madassignment1;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    // initialising fragments & fragment manager
+    HomepageFragment homeFragment = new HomepageFragment();
+    GameSettingsFragment settingsFragment = new GameSettingsFragment();
+    GameSelectionFragment selectionFragment = new GameSelectionFragment();
+    LeaderboardFragment leaderboardFragment = new LeaderboardFragment();
+    LoginFragment loginFragment = new LoginFragment();
+    StatisticsFragment statsFragment = new StatisticsFragment();
+    FragmentManager fm = getSupportFragmentManager();
+
+    // initial app startup
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadLoginFragment();
     }
+
+    // loads homepage
+    private void loadHomepageFragment() {
+        fm.beginTransaction().replace(R.id.MainActivityFrameLayout, homeFragment).commit();
+    }
+
+    // loads settings
+    private void loadSettingsFragment() {
+        fm.beginTransaction().replace(R.id.MainActivityFrameLayout, settingsFragment).commit();
+    }
+
+    // loads game selection
+    private void loadSelectionFragment() {
+        fm.beginTransaction().replace(R.id.MainActivityFrameLayout, selectionFragment).commit();
+    }
+
+    // loads leaderboard
+    private void loadLeaderboardFragment() {
+        fm.beginTransaction().replace(R.id.MainActivityFrameLayout, leaderboardFragment).commit();
+    }
+
+    // loads login
+    private void loadLoginFragment() {
+        fm.beginTransaction().replace(R.id.MainActivityFrameLayout, loginFragment).commit();
+    }
+
+    // loads user statistics
+    private void loadStatsFragment() {
+        fm.beginTransaction().replace(R.id.MainActivityFrameLayout,  statsFragment).commit();
+    }
+
 }
