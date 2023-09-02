c1c3e5f3791e7428902d42e373c1d0f3cf05c0a7
ameliefisher <amelie.fisher@student.curtin.edu.au>
2023-09-02 18:15:00 +0800

    Added code for storing/validating username

--- app/src/main/java/com/example/madassignment1/LoginFragment.java
+++ app/src/main/java/com/example/madassignment1/LoginFragment.java
@@ -11,6 +11,8 @@ import androidx.fragment.app.FragmentManager;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.material.textfield.TextInputEditText;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginFragment extends Fragment {
 
@@ -21,12 +23,33 @@ public class LoginFragment extends Fragment {
         // find the login button
         Button loginButton = rootView.findViewById(R.id.LoginFragmentButton);
 
+        // find the username text box
+        TextInputEditText usernameEditText = rootView.findViewById(R.id.usernameText);
+
         // set a click listener on the login button
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // perform the fragment transaction to load HomepageFragment
-                loadHomepageFragment();
+                // get the username
+                String username = usernameEditText.getText().toString().trim();
+
+                if (!username.isEmpty()){
+                    if (((MainActivity) requireActivity()).usernameExists(username)){
+                        Toast.makeText(requireContext(), "Username already exists", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        // handle the username
+                        ((MainActivity) requireActivity()).handleUsername(username);
+
+                        // perform the fragment transaction to load HomepageFragment
+                        loadHomepageFragment();
+                    }
+                }
+
+                else{
+                    Toast.makeText(requireContext(), "Username cannot be empty!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
